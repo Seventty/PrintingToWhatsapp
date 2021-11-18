d8218845b917b809567f244a9050091f101554f5
seventty <elmastigeron2015@gmail.com>
2021-11-18 12:23:08 -0400

    Share PDF working already

--- app/src/main/java/com/example/printingtowhatsapp/MainActivity.java
+++ app/src/main/java/com/example/printingtowhatsapp/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.printingtowhatsapp;
 
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.pdf.PdfDocument;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -39,6 +49,12 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    int pageHeight = 1120;
+    int pageWidth = 792;
+
+    Bitmap bmp, scaledbmp;
+
+    private static final int PERMISSION_REQUEST_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,10 +64,14 @@ public class MainActivity extends AppCompatActivity {
         WebView webView = findViewById(R.id.webView);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-
         webView.loadUrl("file:///android_asset/index.html");
-
         webView.addJavascriptInterface(new JavaScriptInterface(this), "AndroidFunction");
+
+        if (checkPermission()) {
+            Toast.makeText(this, "Permisos concedidos", Toast.LENGTH_SHORT).show();
+        } else {
+            requestPermission();
+        }
     }
 
     public class JavaScriptInterface {
@@ -63,54 +83,122 @@ public class MainActivity extends AppCompatActivity {
 
         @JavascriptInterface
         public void shareData(String data){
-            Bitmap image = textToBitmap(data);
-            Intent share = new Intent(Intent.ACTION_SEND);
-            share.setType("image/jpeg");
+            bmp = BitmapFactory.decodeResource(getResources(), R.drawable.marlogo);
+            scaledbmp = Bitmap.createScaledBitmap(bmp, 700, 600, false);
 
-            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-            image.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-            File file = new File(Environment.getExternalStorageDirectory() + File.separator + "tmp.jpg");
-            try{
-                file.createNewFile();
-                FileOutputStream fo = new FileOutputStream(file);
-                fo.write(bytes.toByteArray());
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+            //generatePDF(data);
+
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("application/pdf");
+
+            File pdfFile = new File(Environment.getExternalStorageDirectory() + "/ticket.pdf");
+            Uri uri = Uri.fromFile(pdfFile);
+            intent.putExtra(Intent.EXTRA_STREAM, uri);
+
+            try {
+                startActivity(Intent.createChooser(intent, "Compartiendo ticket a imprimir."));
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "Error: No he podido abrir tu archivo de ticket.", Toast.LENGTH_SHORT).show();
             }
 
-            share.putExtra(Intent.EXTRA_STREAM, Uri.parse("file:///tmp.jpg"));
-            startActivity(Intent.createChooser(share, "Share image"));
 
-            Toast.makeText(getApplicationContext(),"Sharing...", Toast.LENGTH_SHORT).show();
+            /*File pdfFile = new File(Environment.getExternalStorageDirectory() + "/Dir/ticket.pdf");
+            Uri path = Uri.fromFile(pdfFile);
+
+            // Setting the intent for pdf reader
+            Intent pdfIntent = new Intent(Intent.ACTION_VIEW);
+            pdfIntent.setDataAndType(path, "application/pdf");
+
+            try {
+                startActivity(pdfIntent);
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(getApplicationContext(), "Can't read pdf file", Toast.LENGTH_SHORT).show();
+            }*/
+
 
             /*Intent sendIntent = new Intent();
             sendIntent.setAction(Intent.ACTION_SEND);
-            sendIntent.putExtra(Intent.EXTRA_TEXT, data);
-            sendIntent.setType("text/plain");
+            sendIntent.putExtra(Intent.EXTRA_TEXT, file.toString());
+            sendIntent.setType("image/png");
 
-            Intent shareIntent = Intent.createChooser(sendIntent, null);
+            Intent shareIntent = Intent.createChooser(sendIntent, "Ticket");
             startActivity(shareIntent);
 
             Toast.makeText(getApplicationContext(),"Sharing...", Toast.LENGTH_SHORT).show();*/
         }
     }
 
-    private Bitmap textToBitmap(String text){
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setTextSize(35);
-        paint.setColor(Color.BLACK);
-        paint.setTextAlign(Paint.Align.LEFT);
+    private void generatePDF(String text) {
+        //Document and page configuration
+        PdfDocument pdfDocument = new PdfDocument();
 
-        float baseline = -paint.ascent();
-        int width = (int) (paint.measureText(text) + 0.5f);
-        int height = (int) (baseline + paint.descent() + 0.5f);
+        Paint paint = new Paint();
+        Paint title = new Paint();
 
-        Bitmap image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(pageWidth, pageHeight, 1).create();
+        PdfDocument.Page myPage = pdfDocument.startPage(pageInfo);
 
-        Canvas canvas = new Canvas(image);
-        canvas.drawText(text, 0, baseline, paint);
-        return image;
+        //Image banner
+        Canvas canvas = myPage.getCanvas();
+        canvas.drawBitmap(scaledbmp, 56, 40, paint);
+
+        //Title text settings
+        title.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
+        title.setTextSize(25);
+        title.setColor(ContextCompat.getColor(this, android.R.color.holo_blue_dark));
+        canvas.drawText("MAR PRINTING SYSTEM", 229, 100, title);
+
+        //Subtitle text settings
+        title.setTextSize(20);
+        title.setColor(ContextCompat.getColor(this, android.R.color.black));
+        canvas.drawText("Ticket de impresion", 229, 70, title);
+
+        //Body text setting
+        title.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+        title.setColor(ContextCompat.getColor(this, R.color.black));
+        title.setTextSize(15);
+        title.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText(text, 396, 560, title);
+
+        //Writing finished
+        pdfDocument.finishPage(myPage);
+
+        //File creation
+        File file = new File(Environment.getExternalStorageDirectory(), "ticket.pdf");
+        try {
+            //End processor
+            pdfDocument.writeTo(new FileOutputStream(file));
+            Toast.makeText(MainActivity.this, "PDF generado con exito.", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //Close pdf thread
+        pdfDocument.close();
+    }
+
+    private boolean checkPermission() {
+        int permission1 = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        int permission2 = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+        return permission1 == PackageManager.PERMISSION_GRANTED && permission2 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0) {
+                boolean writeStorage = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean readStorage = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                if (writeStorage && readStorage) {
+                    Toast.makeText(this, "Permisos concedidos", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permisos denegados.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        }
     }
 }
